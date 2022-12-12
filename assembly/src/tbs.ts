ee98ea11c8a7502a34060bbc4260aeda6e3cf8a7
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2022-12-11 21:01:10 -0800

    feat: order project

--- assembly/src/tbs.ts
+++ assembly/src/tbs.ts
@@ -1,8 +1,32 @@
+const NullID = 0;
+const ObjectID = 1;
+const StringID = 2;
+const ArrayID = 3;
+const NumberID = 4;
 export namespace TBS {
-    export function serialize<T>(data: T): ArrayBuffer {
-        
+    export function serialize<T>(data: T): i32[] {
+        // @ts-ignore
+        if ((isInteger<T>() || isFloat<T>()) && isFinite(data)) {
+            // @ts-ignore
+            return [NumberID, data];
+        } else if (isString<T>()) {
+            // @ts-ignore
+            return [StringID, NullID];
+            // @ts-ignore
+        } else if (isDefined(data.__TBS_Serialize)) {
+            // @ts-ignore
+            return data.__TBS_Serialize();
+        }
+        return unreachable();
     }
-    export function deserialize<T>(data: ArrayBuffer): T {
-
+    export function parse<T>(data: i32[]): T {
+        let type: nonnull<T> = changetype<nonnull<T>>(__new(offsetof<nonnull<T>>(), idof<nonnull<T>>()));
+        // @ts-ignore
+        if (isDefined(type.__TBS_Deserialize)) {
+            // @ts-ignore
+            type.__TBS_Deserialize(data);
+            return type;
+        }
+        return unreachable();
     }
 }
--- assembly/test.ts
+++ assembly/test.ts
@@ -1,12 +1,8 @@
 import { decimalCount32 } from "assemblyscript/std/assembly/util/number";
 import { JSON } from "json-as/assembly";
 import { ASON } from "@ason/assembly";
+import { TBS } from "./src/tbs";
 
-const NullID = 0;
-const ObjectID = 1;
-const StringID = 2;
-const ArrayID = 3;
-const NumberID = 4;
 // ObjectID, KeyID, ValueLength, ...Value, KeyID, ValueLength, ...Value
 @tbs
 class Vec3 {
@@ -19,37 +15,6 @@ const vec: Vec3 = {
     x: 3,
     y: 2,
     z: 8
-}
-
-// @ts-ignore
-@inline
-function serialize<T>(data: T): i32[] {
-    // @ts-ignore
-    if ((isInteger<T>() || isFloat<T>()) && isFinite(data)) {
-        // @ts-ignore
-        return [NumberID, data];
-    } else if (isString<T>()) {
-        // @ts-ignore
-        return [StringID, NullID];
-        // @ts-ignore
-    } else if (isDefined(data.__TBS_Serialize)) {
-        // @ts-ignore
-        return data.__TBS_Serialize();
-    }
-    return unreachable();
-}
-
-// @ts-ignore
-@inline
-function parse<T extends Vec3>(data: i32[]): T {
-    let type: nonnull<T> = changetype<nonnull<T>>(__new(offsetof<nonnull<T>>(), idof<nonnull<T>>()));
-    // @ts-ignore
-    if (isDefined(type.__TBS_Deserialize)) {
-        // @ts-ignore
-        type.__TBS_Deserialize(data);
-        return type;
-    }
-    return unreachable();
 }
 
 // @ts-ignore
@@ -77,20 +42,20 @@ function humanify(data: i32[]): string {
     return result;
 }
 
-const serialized = serialize<Vec3>(vec);
+const serialized = TBS.serialize<Vec3>(vec);
 console.log(`Serialized Vec3: ${serialized.join(" ")}\n${humanify(serialized)}`)
-const deserialized = parse<Vec3>(serialized);
-console.log(humanify(serialize<Vec3>(deserialized)));
+const deserialized = TBS.parse<Vec3>(serialized);
+console.log(humanify(TBS.serialize<Vec3>(deserialized)));
 
 let times = 5_000_000;
 let start = Date.now();
 let warmup = 5_000;
 while (warmup-- > 0) {
-    serialize<Vec3>(vec);
+    TBS.serialize<Vec3>(vec);
 }
 
 while (times-- > 0) {
-    serialize<Vec3>(vec);
+    TBS.serialize<Vec3>(vec);
 }
 
 times = 5_000_000;
@@ -101,11 +66,11 @@ console.log(`Serialize Vec3: ${Date.now() - start}ms`);
 start = Date.now();
 
 while (warmup-- > 0) {
-    parse<Vec3>(serialized);
+    TBS.parse<Vec3>(serialized);
 }
 
 while (times-- > 0) {
-    parse<Vec3>(serialized);
+    TBS.parse<Vec3>(serialized);
 }
 
 times = 5_000_000;
